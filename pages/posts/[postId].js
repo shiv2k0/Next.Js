c0c96bef7d1341e6ccbaa87b0dfdc53e93df0668
Shivendra Kumar Singh <200105055@hbtu.ac.in>
2022-10-29 21:54:35 +0530

    Updated getStaticPath to load all posts with fallback still being false

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -9,21 +9,31 @@ function Post({post}){
 
 export default Post;
 
-export async function getStaticPaths(){
-    return{
-        paths: [
-            {
-                params: {postId: '1'},
-            },
-            {
-                params: {postId: '2'},
-            },
-            {
-                params: {postId: '3'},
-            },
-        ],
-        fallback:false,
+export async function getStaticPaths() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  const paths = data.map(post=>{
+    return {
+        params: {
+            postId: `${post.id}`
+        }
     }
+  })
+  return {
+    // paths: [
+    //   {
+    //     params: { postId: "1" },
+    //   },
+    //   {
+    //     params: { postId: "2" },
+    //   },
+    //   {
+    //     params: { postId: "3" },
+    //   },
+    // ],
+    paths,
+    fallback: false,
+  };
 }
 
 export async function getStaticProps(context){
