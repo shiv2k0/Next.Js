a89d948b61737a9a488f3ed9eb2bd398396ef006
Shivendra Kumar Singh <200105055@hbtu.ac.in>
2022-10-29 11:24:08 +0530

    made Master-Detail pattern using getStaticProps

---
+++ pages/posts/index.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+function PostList({ posts }) {
+  return (
+    <div>
+      <h1>List of Posts</h1>
+      <p>
+        {posts.map((post) => {
+          return (
+            <div key={post.id}>
+              <Link href={`posts/${post.id}`} passHref>
+                <h2>
+                  {post.id} {post.title}
+                </h2>
+              </Link>
+            </div>
+          );
+        })}
+      </p>
+    </div>
+  );
+}
+
+export default PostList;
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  console.log(data);
+
+  return {
+    props: {
+      posts: data.slice(0, 3),
+    },
+  };
+}
