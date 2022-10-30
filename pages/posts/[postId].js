ba6a0ad1064f0c8e3304ca9bca16ae7a5c020cee
Shivendra Kumar Singh <200105055@hbtu.ac.in>
2022-10-30 18:46:55 +0530

    changed fallback to true & enabled error page

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,4 +1,13 @@
+import {useRouter } from 'next/router'
+
 function Post({post}){
+    
+    const router= useRouter()
+
+    if(router.isFallback){
+        return <h1>Loading</h1>
+    }
+
     return(
         <>
             <h2>{post.id} {post.title}</h2>
@@ -12,27 +21,30 @@ export default Post;
 export async function getStaticPaths() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await response.json();
-  const paths = data.map(post=>{
-    return {
-        params: {
-            postId: `${post.id}`
-        }
-    }
-  })
+
+    
+
+//   const paths = data.map(post=>{
+//     return {
+//         params: {
+//             postId: `${post.id}`
+//         }
+//     }
+//   })
   return {
-    // paths: [
-    //   {
-    //     params: { postId: "1" },
-    //   },
-    //   {
-    //     params: { postId: "2" },
-    //   },
-    //   {
-    //     params: { postId: "3" },
-    //   },
-    // ],
-    paths,
-    fallback: false,
+    paths: [
+      {
+        params: { postId: "1" },
+      },
+      {
+        params: { postId: "2" },
+      },
+      {
+        params: { postId: "3" },
+      },
+    ],
+    // paths,
+    fallback: true,
   };
 }
 
@@ -40,11 +52,17 @@ export async function getStaticProps(context){
     const {params}= context
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
     const data = await response.json()
-    console.log(data)
+    console.log(`Generating page for /posts/${params.postId}`)
 
+    if(!data.id){
+        return {
+            notFound: true,
+        }
+    }
     return {
         props:{
             post:data,
+            
         }
     }
 
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -30,7 +30,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts: data,
+      posts: data.slice(0,3),
     },
   };
 }
