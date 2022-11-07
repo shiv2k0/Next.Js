0cef276d703141afea4bed153b2c87e31564b438
Shivendra Kumar Singh <200105055@hbtu.ac.in>
2022-11-07 09:30:47 +0530

    Set_to_Default

--- components/layout/Footer.js
+++
@@ -1,5 +0,0 @@
-function Footer() {
-    return <div className="layout-footer">Footer</div>;
-  }
-  export default Footer;
-  
--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,7 @@
-import "../styles/globals.css";
-import Head from 'next/head'
-// import Header from "components/layout/Header";
-// import Footer from "components/layout/Footer";
-//  {we can use this formate with baseUrl}
-
-import Header from "@/layout/Header";
-import Footer from "@/layout/Footer";
-// {we can use this formate with path}
-import 'styles/layout.css'
-import 'styles/container.css'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  if(Component.getLayout){
-    return Component.getLayout(<Component {...pageProps} />)
-  }
-  return (
-    <>
-      <Head>
-        <title>CodeWithMe</title> 
-        <meta name='description' content={` Coming soon`} />
-        {/* adding title and meta in _app.js will it for all the pages available unless specially added in any page */}
-      </Head>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
